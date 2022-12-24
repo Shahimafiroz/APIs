43b8067bfe50c770297f8b22d5eecf7f991ed04a
SHAHIMA FIROZ KHAN <99420269+Shahimafiroz@users.noreply.github.com>
2022-12-24 21:52:11 +0530

    hey

--- Weather_App/app.js
+++ Weather_App/app.js
@@ -1,17 +1,45 @@
+const bp = require("body-parser");
 const exp = require("express");
-// var body_par = require("body-parser");
-
+const https = require("https");
 const app = exp();
 
-// app.use(body_par.urlencoded({ extended: true }));
+app.use(bp.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-  res.send("server is up and running");
+  res.sendFile(__dirname + "/index.html");
 });
 
-// app.post("/", function (res, req) {
-//   res.send("hi");
-// });
+app.post("/", function (req, res) {
+  console.log(req.body.cityName);
+  const apiKey = "3c86e64ecb78355d03b5dd63812fe9a8";
+  const query = req.body.cityName;
+  const units = "metric";
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?appid=" +
+    apiKey +
+    "&units=" +
+    units +
+    "&q=" +
+    query;
+  https.get(url, function (resp) {
+    resp.on("data", function (data) {
+      console.log(resp.statusCode);
+
+      const weather_data = JSON.parse(data);
+      const temp = weather_data.main.temp;
+      const des = weather_data.weather[0].description;
+      const location = weather_data.name;
+      const icon = weather_data.weather[0].icon;
+      const iconurl = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+      res.write(
+        "<h1>weather in " + location + " is currently " + des + " like .</h1>"
+      );
+      res.write("<h1>temperature is " + temp + "</h1>");
+      res.write("<img src = " + iconurl + ">");
+      res.send();
+    });
+  });
+});
 
 app.listen(3000, function () {
   console.log("server is running");
