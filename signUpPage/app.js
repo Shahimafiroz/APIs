093cd28a4981a4db2d775866dacbd5f37d6cbb56
SHAHIMA FIROZ KHAN <99420269+Shahimafiroz@users.noreply.github.com>
2022-12-25 18:54:14 +0530

    front end complete

--- signUpPage/app.js
+++ signUpPage/app.js
@@ -1,7 +1,19 @@
+//jshint esversion: 6
+
 var exp = require("express");
+var bp = require("body-parser");
+var request = require("request");
 
 const app = exp();
 
-app.get("/", function (res, req) {
-  res.sendFile(__dirname + "index.html");
+app.use(exp.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {});
+
+app.listen(3000, function () {
+  console.log("server is running");
 });
