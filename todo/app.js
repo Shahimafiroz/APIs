c251df100baf6be4874ccb001fb0492b64e07888
SHAHIMA FIROZ KHAN <99420269+Shahimafiroz@users.noreply.github.com>
2023-01-09 15:22:15 +0530

    5

--- todo/app.js
+++ todo/app.js
@@ -6,26 +6,14 @@ app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
   var today = new Date();
-  var currentDate = today.getDay();
-  var day = "";
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
 
-  if (currentDate === 0) {
-    day = " Sunday ";
-  } else if (currentDate === 1) {
-    day = "Monday";
-  } else if (currentDate === 2) {
-    day = "Tuesday";
-  } else if (currentDate === 3) {
-    day = "wednesday";
-  } else if (currentDate === 4) {
-    day = "thursday";
-  } else if (currentDate === 5) {
-    day = "friday";
-  } else {
-    day = "saturday";
-  }
-
-  res.render("list", { kindOfDay: day });
+  var day = today.toLocaleDateString("en-US", options);
+  res.render("list.ejs", { kindOfDay: day });
 });
 
 app.listen(3000, function () {
