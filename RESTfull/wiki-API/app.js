fb2fa6a70a9b5979e046279c52dea527da42ae25
SHAHIMA FIROZ KHAN <99420269+Shahimafiroz@users.noreply.github.com>
2023-03-22 01:02:02 +0530

    REST SET UP complete

--- RESTfull/wiki-API/app.js
+++ RESTfull/wiki-API/app.js
@@ -1,11 +1,28 @@
-const exp = require("express");
+// require everything
+const express = require("express");
 const bp = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
-
-const app = exp();
-
+//express used
+const app = express();
+//body-parser used
 app.use(bp.urlencoded({ extended: true }));
+// Ejs used
+app.set("view engine", "ejs");
+// Mongoose Used
+mongoose.set("strictQuery", false);
+mongoose.connect("mongodb://localhost:27017/WikiDB", { useNewUrlParser: true });
+/////////////////////////////////////////////////------Backend Begins--------///////////////////////////////////////////////////////////////////
+//mongoose schema
+const articleSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+//mongoose model
+const article = mongoose.model("article", articleSchema);
+//mongoose
+
+app.use(express.static("public"));
 
 app.listen(3000, function () {
   console.log("server is running");
